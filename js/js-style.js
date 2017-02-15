64adfc0fe30773204cc90c7374e983eff07584df
BAXtheGREATEST <baxthegreatest@yandex.ru>
2017-02-15 12:45:31 +0200

    color in form

--- js/js-style.js
+++ js/js-style.js
@@ -1,7 +1,18 @@
 $(document).ready(
+    $(document).on('keyup', '.user-phone--js', function(){
+        var typedPhone = $(this).val();
+        if (!typedPhone.match(/^[0-9]*$/)) {
+            $(this).addClass('error');
+        } else {
+            $(this).removeClass('error');
+        }
+    }),
+
     $('#user-recall').submit(function(e) {
         var userPhone = document.querySelector('.user-phone--js'),
-            userName = document.querySelector('.user-name--js');
+            userName = document.querySelector('.user-name--js'),
+            selectedColor = document.querySelector('.visuallyhidden:checked');
+
         if(!userPhone.value || !userName.value) {
             $.ajax({
                 url: "https://formspree.io/baxthegreatest@gmail.com",
